9b6b378688ca7d8b0ed0da34896933eb58a05cef
fyujiwara <imp.yfujiwara+github@gmail.com>
2021-02-07 21:12:49 +0900

    SimpleTextのnamespaceを追加

--- lib/good/todo_app.rb
+++ lib/good/todo_app.rb
@@ -2,9 +2,9 @@
 
 require_relative 'command/export_all_todo_command'
 require_relative 'model/todo_formatter/formatter_resolver'
-require_relative 'model/todo_formatter/calendar_todo_formatter'
-require_relative 'model/todo_formatter/kanban_todo_formatter'
-require_relative 'model/todo_formatter/todo_formatter'
+require_relative 'model/todo_formatter/simple_text/calendar_todo_formatter'
+require_relative 'model/todo_formatter/simple_text/kanban_todo_formatter'
+require_relative 'model/todo_formatter/simple_text/todo_formatter'
 require_relative 'entity/todo'
 require_relative 'entity/calendar_todo'
 require_relative 'entity/kanban_todo'
